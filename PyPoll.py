084d30fb8c7cec891303972d4a9667223dc5b58c
Mohammed Cheentavida <user.name>
2022-10-17 17:44:28 -0400

    last modification to pypoll.py

--- PyPoll.py
+++ PyPoll.py
@@ -25,6 +25,30 @@ print(f"\n Total votes {str(rowcount)}")
 print(f" Candidate {sorted(candidate.items(), key = lambda item: item[1])[-1]} is the winner")
 #print(sorted(candidate.values(),reverse=True)[0])
 print(candidate) # display
-for name, votes in candidate.items():
-    # find the % votes by candidate
-    print(f"Candidate {name} received {(votes/rowcount)*100} %")
+
+total_votes = rowcount    
+winning_candidate = sorted(candidate.items(), key = lambda item: item[1])[-1][0]#
+winning_count = sorted(candidate.items(), key = lambda item: item[1])[-1][1]
+winning_percentage = (winning_count/rowcount)* 100
+print(f"winning Candidate {winning_candidate}  Received {winning_count} votes and  {winning_percentage} % of votes")
+election_result = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")    
+with open("election_analysis.txt",mode="w+") as election_data:
+     election_data.write(election_result)
+     for name, votes in candidate.items():
+       # find the % votes by candidate
+       print(f"Candidate {name} received {(votes/rowcount)*100} %")
+       election_data.write(f"{name}: {(votes/rowcount)*100:.1f} % ({votes:,}) \n")
+     #file_pointer = csv.write(election_data)
+    
+     election_data.write(winning_candidate_summary)
+     
